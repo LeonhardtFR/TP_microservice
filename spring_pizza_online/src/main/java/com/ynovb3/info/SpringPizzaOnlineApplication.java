b323cb3b605fc0e573fd2f890858f65e30dce566
Maxime <maxime.beziat@free.fr>
2021-11-16 14:15:28 +0100

    Creation du spring

---
+++ spring_pizza_online/src/main/java/com/ynovb3/info/SpringPizzaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.ynovb3.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPizzaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPizzaOnlineApplication.class, args);
+	}
+
+}
---
+++ spring_pizza_online/src/test/java/com/ynovb3/info/SpringPizzaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynovb3.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
